6c52ab68398c00fc8d275f96a5ca6dc583f15386
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-26 17:18:47 +0200

    chore: project setup for reviews app

---
+++ screens/reviewDetails.js
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function ReviewDetails() {
+    return (
+        <View style={styles.container}>
+            <Text>Review Details screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24
+    }
+});
