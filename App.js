be4f5f00cbd6f8044bebb2c29abd11c261631558
alain-kubwayo <ahkubwayo@gmail.com>
2023-05-27 16:10:15 +0200

    feat: add nunito custom font

--- App.js
+++ App.js
@@ -1,15 +1,35 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text } from "react-native";
+import * as Font from "expo-font";
 
-import Home from './screens/Home';
+import Home from "./screens/Home";
+import AppLoading from "expo-app-loading";
+
+import { useState } from "react";
+
+const getFonts = () =>
+  Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) {
     return (
-        <View>
-            <Home />
-        </View>
-    )
+      <View>
+        <Home />
+      </View>
+    );
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-
-})
+const styles = StyleSheet.create({});
